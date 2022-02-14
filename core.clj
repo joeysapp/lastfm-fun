186a8d18e3d03364a14520b300e468d6ca846130
joeysapp <sapp.joey@gmail.com>
2022-02-13 22:19:36 -0600

    Renaming proj, (time) lazy/eager loading

--- core.clj
+++ core.clj
@@ -1,11 +1,15 @@
-(ns proj-002.core
+(ns lastfm-fun.core
   (:require [clojure.java.io :as io]
             [clojure.data.csv :as csv]))
 (defn -main [& args])
 
-(defn take-csv [f] (with-open [r (io/reader f)] (doall (csv/read-csv r))))
-;(def hist (rest (take-csv "spotify-history-2k.csv"))) ;([artist,album,track,track-id,date])
-(def hist (rest (take-csv "spotify-history-300k.csv")))) ; Jan 2014 to Dec 2021
+(defn take-csv [fname] (with-open [file (io/reader fname)] (doall (csv/read-csv file)))))
+(def hist (rest (take-csv "spotify-history-xl.csv"))) ; Jan 2014 to Dec 2021
+;(def hist (rest (take-csv "spotify-history-sm.csv")))) ; 2000 scrobbles
+
+;; Lazy
+;; (defn take-csv [fname] (with-open [file (io/reader fname)] (csv/read-csv (slurp file))))
+;; (def hist (rest (take-csv "spotify-history-xl.csv"))) ; Jan 2014 to Dec 2021
 
 (defn get-artists []
   "Returns set of artists"
@@ -15,8 +19,23 @@
   "Returns ([plays]) by artist"
   (filter (fn [p] (= (first p) a)) hist))
 
+;; This is very slow. 
+;; - Is it okay being slow?
+;; - If is it something that can just stay loaded in, or do
+;; - we want to re-compute? (think in lists...)
+;; BENCHMARKING too
 ; Get a map of {:artist play count} ;; {:Coldplay 420 :Washed Out 69}
-(into {} (for [a (get-artists)] {(keyword a) (count (get-artist-plays a))}))  
+;; Non-lazy, 197 seconds... Lazy, 190 seconds
+(time (into {} (for [a (get-artists)] {(keyword a) (count (get-artist-plays a))})))
+
+(transduce
+ (partition-by identity)
+ (fn 
+   ([] []) ;; init - returns initial value for accumulator
+   ([acc] acc)    ;; completion - returns the final result, take the final accumulated value
+   ([acc e] (conj acc e)))    ;; step - returns accumulated state and takes accumulated state from before and new element
+ '()
+[[1 "foo"] [2 "bar"] [3 "whee"] [1 "???"]])
 
 ;; Experimenting
 (let [artists #{}]
